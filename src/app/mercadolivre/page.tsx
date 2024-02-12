0bebdf6ea8b9cbe1b1e143577c3bc53ebd37904e
PETRUS NOLETO <88741218+PetrusNoleto@users.noreply.github.com>
2024-02-12 20:53:09 -0300

    up

--- src/app/mercadolivre/page.tsx
+++ src/app/mercadolivre/page.tsx
@@ -1,7 +1,15 @@
+'use client'
+import { initMercadoPago, Wallet } from '@mercadopago/sdk-react'
+
+
 const MercadoLivrePage = ()=>{
+    initMercadoPago('APP_USR-3f7ac6d1-1c76-4664-970e-f398327f2c51');
     return(
         <div>
-             
+                <div id="wallet_container"></div>
+                
+                <Wallet initialization={{ preferenceId: '<PREFERENCE_ID>' }} customization={{ texts:{ valueProp: 'smart_option'}}} />
+
         </div>
     )
 }
